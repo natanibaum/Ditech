7ab18a36f642bab1739020d6799af78d46465e35
Natani Baum <Natani Baum>
2016-08-06 13:29:57 -0300

    Estrutura básica do layout.

---
+++ layout/header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title><?php if(isset($title)){ echo $title; }?></title>
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<br>
+<center><img height="100px"width="30%"src="img/ditech.png"/></center>
+<br>
